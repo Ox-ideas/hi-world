3b47a1d1ef464b3446ad533e144b34d0576d500f
ox-ideas <sunnygb.8@gmail.com>
2018-08-14 21:14:48 +0800

    base .netcore console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myapp.px
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
