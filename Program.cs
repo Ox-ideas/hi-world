bc4ae03caf03587c61859635108b6d57ce3fc6ad
ox-ideas <sunnygb.8@gmail.com>
2018-08-15 10:59:49 +0800

    add fibonacci numbers

--- Program.cs
+++ Program.cs
@@ -22,6 +22,24 @@ namespace myapp.px
             Console.WriteLine();
             Console.WriteLine($"My name is {names[0]}");
             Console.WriteLine($"Say hello to {names[1]} and {names[2]}");
+
+            #region fibonacci
+                var fibonacci_nos = new List<int> {1,1};
+
+                while(fibonacci_nos.Count < 20){
+                    var prev = fibonacci_nos[fibonacci_nos.Count -1];
+                    var prev2 = fibonacci_nos[fibonacci_nos.Count -2];
+
+                    fibonacci_nos.Add(prev + prev2);
+                }
+                // display first 20 numbers
+                /*for(int i = 0; i < 20; i++){
+                    Console.WriteLine(fibonacci_nos[i]);
+                }*/
+                foreach(var item in fibonacci_nos){
+                    Console.WriteLine(item);
+                }
+            #endregion
         }
     }
 }
